4fbefabb3ee671c267a0e3cd770c5c11a8ca2021
shubham1 <rks.malhotra0002@gmail.com>
2019-12-02 17:36:06 +0530

    Toaster Message

--- app/src/main/java/com/arjun/toastylibrary/MainActivity.java
+++ app/src/main/java/com/arjun/toastylibrary/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.arjun.toastlibrary.ToasterMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToasterMessage.s(this,"shubham malhotra");
+
     }
 }
